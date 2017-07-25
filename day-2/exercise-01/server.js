e007310b56eb4ec1de4e669009b571faa6dbcf81
Edy Segura <edysegura@gmail.com>
2017-07-25 19:45:52 -0300

    Solved day 2 exercise 01

--- day-2/exercise-01/server.js
+++ day-2/exercise-01/server.js
@@ -12,9 +12,50 @@ app.use(express.static('public'))
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
+//data
+const data = {
+    title: 'Interest Rate Calculator',
+    value: null,
+    rate: null,
+    months: null,
+    amount: null,
+    projection: []
+}
+
+//business logic
+const calculateInterest = (p, r, t) => p * Math.pow( 1 + r, t)
+
+const fillData = ({value, rate, months}) => {
+    data.value = parseFloat(value) || null
+    data.rate = parseFloat(rate) || null
+    data.months = parseFloat(months) || null
+    data.projections = []
+}
+
+const projections = (data) => {
+    const t = data.months
+    const monthArray = Array.from(new Array(t), (t, i) => i + 1)
+
+    data.projections = monthArray.map(month => {
+        const amount = calculateInterest (
+            data.value,
+            data.rate / 100,
+            month
+        )
+        return { month, amount }
+    })
+}
+
+//routes
 app.get('/', (request, response) => {
-    const title = 'Interest Rate Calculator'
-    response.render('index', { title })
+    fillData({})
+    response.render('index', data)
 })
 
-app.listen(PORT, error => console.log('Server running at ' + PORT))
+app.get('/calculate', (request, response) => {
+    fillData(request.query)
+    projections(data)
+    response.render('index', data)
+})
+
+app.listen(PORT, error => console.log('Server running on PORT: ' + PORT))
